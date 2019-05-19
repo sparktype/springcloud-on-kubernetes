ce2963c7f5f1c9250235b49eb1dae7c2023b5e88
Leonrdo Park <ageofblue@gmail.com>
2019-05-19 17:03:35 +0900

    Fix springcloud-config

--- springcloud-config/src/main/java/clustercamp/springcloud/config/Application.java
+++ springcloud-config/src/main/java/clustercamp/springcloud/config/Application.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+
 @EnableConfigServer
+@SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
