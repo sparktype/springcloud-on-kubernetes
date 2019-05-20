bdade74b1ce88b3a4391626627c1c06a1faf57db
Leonrdo Park <ageofblue@gmail.com>
2019-05-21 00:31:10 +0900

    Edit start security

--- springcloud-zuul/src/main/java/clustercamp/springcloud/zuul/Application.java
+++ springcloud-zuul/src/main/java/clustercamp/springcloud/zuul/Application.java
@@ -2,12 +2,10 @@ package clustercamp.springcloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
+@SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
