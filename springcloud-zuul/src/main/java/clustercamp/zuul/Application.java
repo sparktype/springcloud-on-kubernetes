326e538fc8a5c8bc7b0e9dac6309543e1d0b6679
Leonrdo Park <ageofblue@gmail.com>
2019-06-02 01:08:43 +0900

    Add zuul

--- springcloud-zuul/src/main/java/clustercamp/zuul/Application.java
+++ springcloud-zuul/src/main/java/clustercamp/zuul/Application.java
@@ -2,9 +2,11 @@ package clustercamp.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
