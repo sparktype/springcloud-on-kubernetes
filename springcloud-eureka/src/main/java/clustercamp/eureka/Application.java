6edeeeff66cd99f0be912f26293ae3ca1e135773
Leonrdo Park <clustercamp@gmail.com>
2019-06-18 23:01:24 +0900

    Add springboot admin

--- springcloud-eureka/src/main/java/clustercamp/eureka/Application.java
+++ springcloud-eureka/src/main/java/clustercamp/eureka/Application.java
@@ -3,13 +3,15 @@ package clustercamp.eureka;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
 @EnableEurekaServer
 @EnableAdminServer
-@EnableAutoConfiguration
 public class Application {
 
   public static void main(String[] args) {
