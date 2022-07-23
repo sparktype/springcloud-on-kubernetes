0370cd503f56c4fe96988048007684298dddac49
SangSun Park <spark@cncf.io>
2022-07-23 15:18:33 +0900

    Add helm components
    Took 1 hour 16 minutes

--- helm/consul/setup.sh
+++
@@ -1,5 +0,0 @@
-helm repo add hashicorp https://helm.releases.hashicorp.com
-helm install consul hashicorp/consul -f values.yaml -n devops
-
-
-kubeclt export
--- helm/strimzi/setup.sh
+++
@@ -1,5 +0,0 @@
-helm repo add strimzi https://strimzi.io/charts/
-
-helm install strimzi strimzi/strimzi-kafka-operator -n kafka
-
-kubectl apply -f kafka.yaml -n kafka
